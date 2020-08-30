1a62ef159becbd93f935d461bf58c47660bb4aff
Anton Rochniak <rochniak15@gmail.com>
2020-08-30 15:57:30 +0300

    Implement login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,13 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import crypto from 'crypto'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.body?.username !== 'ddi') {
+  const body: { username: string; password: string } = JSON.parse(req.body)
+  if (body.username !== 'ddi') {
     res.statusCode = 400
     res.json({ error: 'user not found' })
     return
   }
 
-  if (!req.body?.password) {
+  if (!body.password) {
     res.statusCode = 400
     res.json({ error: 'password is required' })
     return
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,89 @@
 import { Paper, Grid, TextField, Button } from '@material-ui/core'
+import { useState, useCallback, useRef } from 'react'
+import { useRouter } from 'next/router'
 
 const Home = (): JSX.Element => {
-  // const handleSubmit = (): void => {
-  // make an api request for an http://localhost:3000/api/login endpoint
-  // body request example { username: 'test', password: 'test }
-  // please, display server errors somewhere
-  // username 'ddi' will return token, otherwise -- error message
-  // }
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [isError, setIsError] = useState(false)
+
+  const router = useRouter()
+
+  const usernameInputRef = useRef<HTMLInputElement>()
+
+  const userNameChangeHandler = useCallback((e: React.MouseEvent<HTMLInputElement>) => {
+    if (isError) setIsError(false)
+    const username = e.currentTarget.value
+    setUsername(username)
+  })
+
+  const passwordChangeHandler = useCallback((e: React.MouseEvent<HTMLInputElement>) => {
+    const password = e.currentTarget.value
+    setPassword(password)
+  })
+
+  const errorHandler = useCallback(() => {
+    setIsError(true)
+    usernameInputRef.current.focus()
+    usernameInputRef.current.setSelectionRange(0, usernameInputRef.current.value.length)
+  })
+
+  const submitHandler = useCallback(async (): void => {
+    const body = JSON.stringify({ username, password })
+    const response = await fetch('http://localhost:3000/api/login', { method: 'POST', body })
+    if (response.ok) {
+      const json: { token: string } = await response.json()
+      localStorage.setItem('token', json.token)
+      router.push('/table')
+    } else {
+      errorHandler()
+    }
+  })
+
+  const keyDownHandler = useCallback((e: React.KeyboardEvent) => {
+    if (e.key !== 'Enter' || (e.target as HTMLElement).tagName === 'BUTTON') return
+    submitHandler()
+  })
 
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
-      <Paper style={{ padding: 30 }}>
+      <Paper style={{ padding: 30 }} onKeyDown={keyDownHandler}>
         <Grid container>
           <Grid container spacing={8} alignItems="flex-end">
             <Grid item md={true} sm={true} xs={true}>
-              <TextField id="username" label="Username" type="text" fullWidth autoFocus required />
+              <TextField
+                inputRef={usernameInputRef}
+                error={isError}
+                id="username"
+                label="Username"
+                type="text"
+                onChange={userNameChangeHandler}
+                fullWidth
+                autoFocus
+                required
+              />
             </Grid>
           </Grid>
           <Grid container spacing={8} alignItems="flex-end">
             <Grid item md={true} sm={true} xs={true}>
-              <TextField id="password" label="Password" type="password" fullWidth required />
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                onChange={passwordChangeHandler}
+                fullWidth
+                required
+              />
             </Grid>
           </Grid>
           <Grid container justify="center" style={{ marginTop: '10px' }}>
-            <Button type="submit" variant="outlined" color="primary" style={{ textTransform: 'none' }}>
+            <Button
+              type="submit"
+              onClick={submitHandler}
+              variant="outlined"
+              color="primary"
+              style={{ textTransform: 'none' }}
+            >
               Login
             </Button>
           </Grid>
